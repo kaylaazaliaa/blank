ceed9d2af55c930a3887d57aa7fea88b1f597eec
kaylaazaliaa <kaylazharifah680@gmail.com>
2025-05-27 08:12:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.image ("E3878D5F-D924-4738-B8BA-7885B70E2018.jpeg", width=200)
 
 st.title("Aplikasi Sederhana")
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
-angka = st.number_input("Tulis sebuah Angka:", Value=0, step=1)
+angka = st.number_input("Tulis sebuah Angka:", value=0, step=1)
 
 if (angka % 2) == 0:
     st.writer(f"{angka} adalah Bilangan Genap")
